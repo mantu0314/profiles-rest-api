1ec4ee52a0c11395ac68e7f22d8d27ac8450cb43
Mantu <mantu031495@gmail.com>
2020-08-01 04:07:29 +0530

    Rest ApiView Added

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.views import Response
 
+class HelloApiView(APIView):
+
+    def get(self,request,format=None):
+        an_apiview=['Follwing are the features of rest api',
+                 'function are(delete,update,patch)',
+                 'Gives you the most control over the logic']
+
+        return Response({"messaage":"Hello","an_apiviw":an_apiview})             
 # Create your views here.
